ac7dcce4aed64c0bef9239ff34e08b44ef7fe9c3
Jimmy Ong <112923324+Jake-Sudoku@users.noreply.github.com>
2022-12-14 01:57:23 +0800

    Update Presentation-slides.py

--- Steamlit/Presentation-slides.py
+++ Steamlit/Presentation-slides.py
@@ -20,7 +20,7 @@ st.title('🦟💀 Project 4 - West Nile Virus Prediction')
 
 with st.sidebar:
     choose = option_menu("Directory",[ "About","Slides", "Interactive"],
-                         icons=['people', 'file earmark slides', 'bar chart'],
+                         icons=['people', 'file earmark slides', 'bar-chart'],
                          menu_icon="app-indicator", default_index=0,
                          styles={
         "container": {"padding": "5!important", "background-color": "#fafafa"},
