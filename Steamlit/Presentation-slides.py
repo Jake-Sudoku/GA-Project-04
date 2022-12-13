5bb900171291575feeb7e9ce9bb7a6a2486bc672
Jimmy Ong <112923324+Jake-Sudoku@users.noreply.github.com>
2022-12-14 01:28:32 +0800

    Update Presentation-slides.py

--- Steamlit/Presentation-slides.py
+++ Steamlit/Presentation-slides.py
@@ -66,10 +66,10 @@ elif choose == "Interactive":
     
     # Load data
     def data_train(): 
-    df = pd.read_csv('./train_cleaned.csv')
+        df = pd.read_csv('./train_cleaned.csv')
     return df
     def data_spray(): 
-    df = pd.read_csv('./spray_cleaned.csv')
+        df = pd.read_csv('./spray_cleaned.csv')
     return df
 
     df_train=data_train()
