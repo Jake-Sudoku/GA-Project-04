7b5a6c19ad614c63f71d63d9473510f87b649adb
Jimmy Ong <112923324+Jake-Sudoku@users.noreply.github.com>
2022-12-14 01:27:35 +0800

    Update Presentation-slides.py

--- Steamlit/Presentation-slides.py
+++ Steamlit/Presentation-slides.py
@@ -63,6 +63,7 @@ elif choose == "Slides":
 
 
 elif choose == "Interactive":
+    
     # Load data
     def data_train(): 
     df = pd.read_csv('./train_cleaned.csv')
@@ -70,22 +71,24 @@ elif choose == "Interactive":
     def data_spray(): 
     df = pd.read_csv('./spray_cleaned.csv')
     return df
-    
+
     df_train=data_train()
     df_spray=data_spray()
-    
+
     #Spray Locations from 2011-2013
     MAPBOX_TOKEN = 'pk.eyJ1IjoibWFyaWVkcmV6IiwiYSI6ImNsOXl5dTFtZjAyYm4zd28zN3Y1ZzYycm0ifQ.W1Toe6X5S9AELY56h0OQDw'
     px.set_mapbox_access_token(MAPBOX_TOKEN)
     fig = px.scatter_mapbox(df_spray, lat = 'latitude', lon  = 'longitude',animation_frame="date",size_max=15, zoom = 10)
 
     fig.update_layout(title = 'Spray Locations from 2011-2013',
-        autosize=False,
-        width=500,
-        height=700,mapbox_accesstoken=MAPBOX_TOKEN,mapbox_style="light")
+                      autosize=False,
+                      width=500,
+                      height=700,
+                      mapbox_accesstoken=MAPBOX_TOKEN,mapbox_style="light")
 
     fig.show()
     st.plotly_chart(fig, use_container_width=True)
+    
     
     #Traps Locations from 2007-2013
     MAPBOX_TOKEN = 'pk.eyJ1IjoibWFyaWVkcmV6IiwiYSI6ImNsOXl5dTFtZjAyYm4zd28zN3Y1ZzYycm0ifQ.W1Toe6X5S9AELY56h0OQDw'
