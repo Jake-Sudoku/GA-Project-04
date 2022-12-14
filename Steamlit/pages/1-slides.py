95507807e232ddd6416114852de5a0620e4de7d3
Jimmy Ong <112923324+Jake-Sudoku@users.noreply.github.com>
2022-12-14 21:33:19 +0800

    Update 1-slides.py

--- Steamlit/pages/1-slides.py
+++ Steamlit/pages/1-slides.py
@@ -18,5 +18,5 @@ def show_pdf(file_path):
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
         pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
-
-show_pdf('./test-GA-Project-4.pdf')
+        
+        show_pdf('./test-GA-Project-4.pdf')
