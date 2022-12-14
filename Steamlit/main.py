b1fd390316ce0f2c2315fac5480096f6e2cbf050
Jimmy Ong <112923324+Jake-Sudoku@users.noreply.github.com>
2022-12-14 22:29:50 +0800

    Update main.py

--- Steamlit/main.py
+++ Steamlit/main.py
@@ -53,8 +53,8 @@ elif choose == "Slides":
 elif choose == "Interactive":
     
     # Load data
-    df_train = pd.read_csv('traincleaned.csv')
-    df_spray = pd.read_csv('spraycleaned.csv')
+    df_train = pd.read_csv("traincleaned.csv")
+    df_spray = pd.read_csv("spraycleaned.csv")
 
     #Spray Locations from 2011-2013
     MAPBOX_TOKEN = 'pk.eyJ1IjoibWFyaWVkcmV6IiwiYSI6ImNsOXl5dTFtZjAyYm4zd28zN3Y1ZzYycm0ifQ.W1Toe6X5S9AELY56h0OQDw'
