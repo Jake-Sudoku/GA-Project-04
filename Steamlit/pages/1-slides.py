12c59ff9aa89baa23ddb6f27c7f9e21aa9c93988
Jimmy Ong <112923324+Jake-Sudoku@users.noreply.github.com>
2022-12-14 21:58:45 +0800

    Update 1-slides.py

--- Steamlit/pages/1-slides.py
+++ Steamlit/pages/1-slides.py
@@ -21,8 +21,8 @@ st.title('🌷 Slides')
 def show_pdf(file_path):
     with open(file_path,"rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-        
+        #pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+        pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
         # Displaying File
         st.markdown(pdf_display, unsafe_allow_html=True)
         
