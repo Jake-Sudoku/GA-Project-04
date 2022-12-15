6d2a067f0349e329420c54322ba9ca9124c94271
Jimmy Ong <112923324+Jake-Sudoku@users.noreply.github.com>
2022-12-15 13:41:15 +0800

    Update streamlit_app.py

--- Steamlit-test/streamlit_app.py
+++ Steamlit-test/streamlit_app.py
@@ -38,8 +38,8 @@ font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;}
  </style> """, unsafe_allow_html=True)
 st.markdown('<p class="font">About</p>', unsafe_allow_html=True)
 st.write("This project is done to as part of General's Assembly (GA) requirement to pass the course. \nOur Group Members : \n\nTan Ming Jie \n\nLiam Ting Wei \n\nMaryam \n\nPriscilla Ong \n\nJimmy Ong")    
- #st.image(profile, width=700 )
-    st.sidebar.markdown("# Page 3 🎉")
+#st.image(profile, width=700 )
+st.sidebar.markdown("# Page 3 🎉")
 
 page_names_to_funcs = {
     "Main Page": main_page,
