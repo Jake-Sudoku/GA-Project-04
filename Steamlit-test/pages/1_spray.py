92e0d454a8babb1dc6b84628e6de12929f3c45ce
Jimmy Ong <112923324+Jake-Sudoku@users.noreply.github.com>
2022-12-15 14:18:50 +0800

    Update 1_spray.py

--- Steamlit-test/pages/1_spray.py
+++ Steamlit-test/pages/1_spray.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Set Page configuration
 # Read more at https://docs.streamlit.io/1.6.0/library/api-reference/utilities/st.set_page_config
-st.set_page_config(page_title='Project 4 - West Nile Virus Prediction', page_icon='🦟', layout='wide', initial_sidebar_state='expanded')
+st.set_page_config(page_title='Number of Mosquitos in areas with wnvpresent', page_icon='🦟', layout='wide', initial_sidebar_state='expanded')
 
 # Set title of the app
 st.title('🦟💀 Project 4 - West Nile Virus Prediction')
@@ -32,16 +32,32 @@ df_spray_csv = Path(__file__).parents[0] / 'spray_cleaned.csv'
 df_train = pd.read_csv(df_train_csv)
 df_spray = pd.read_csv(df_spray_csv)
 
-#Spray Locations from 2011-2013
+#Number of Mosquitos in areas with wnvpresent
 MAPBOX_TOKEN = 'pk.eyJ1IjoibWFyaWVkcmV6IiwiYSI6ImNsOXl5dTFtZjAyYm4zd28zN3Y1ZzYycm0ifQ.W1Toe6X5S9AELY56h0OQDw'
 px.set_mapbox_access_token(MAPBOX_TOKEN)
-fig = px.scatter_mapbox(df_spray, lat = 'latitude', lon  = 'longitude',animation_frame="date",size_max=15, zoom = 10)
 
-fig.update_layout(title = 'Spray Locations from 2011-2013',
-                  autosize=False,
-                  width=500,
-                  height=700,
-                  mapbox_accesstoken=MAPBOX_TOKEN,mapbox_style="light")
+#Count the number of mosquitos in areas
+mosquito_count = df_train.groupby(['address'], as_index = False)[['nummosquitos']].sum()
+#Group the area by address and use the median address
+areas = df_train.groupby(['address'], as_index = False)[['latitude','longitude']].median()
+
+#Group the wnvpresent by address
+wnv = df_train.groupby(['address'], as_index = False)[['wnvpresent']].sum() 
+
+#Merge mosquitocount , areas and wnvpresent together
+mosquito_areas_wnv = pd.concat([mosquito_count,areas, wnv], axis = 1)
+
+#Drop the address as it is not required
+mosquito_areas_wnv.drop('address', axis = 1, inplace = True)
+
+fig = px.scatter_mapbox(mosquito_areas_wnv, lat = 'latitude', lon  = 'longitude', color = 'wnvpresent',
+                        size = 'nummosquitos', color_continuous_scale=px.colors.cyclical.Edge,
+                        hover_data = ['nummosquitos', 'wnvpresent'],
+                       zoom = 9,
+                       width=500,
+                       height=700,
+                       title="Number of Mosquitos in areas with wnvpresent")
+
 fig.show()
 st.plotly_chart(fig, use_container_width=True)
     
