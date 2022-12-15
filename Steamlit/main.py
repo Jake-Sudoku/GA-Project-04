da59b0a70b7911a7a91f27bae84f49e02cf91653
Jimmy Ong <112923324+Jake-Sudoku@users.noreply.github.com>
2022-12-15 16:08:29 +0800

    Update main.py

--- Steamlit/main.py
+++ Steamlit/main.py
@@ -53,7 +53,6 @@ font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;}
 st.markdown('<p class="font">About</p>', unsafe_allow_html=True)
 st.write("This project is done to as part of General's Assembly (GA) requirement to pass the course. \n\nDSI33 Group Members : \n\nTan Ming Jie \n\nLiam Ting Wei \n\nMaryam \n\nPriscilla Ong \n\nJimmy Ong")    
 
-st.set_page_config(page_title='Project 4 - West Nile Virus Prediction', page_icon='🦟', layout='wide', initial_sidebar_state='expanded')
 
 # Set title of the app
 st.title('🦟💀 Project 4 - West Nile Virus Prediction')
