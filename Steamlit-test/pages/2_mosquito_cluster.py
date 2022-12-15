c40f82b8d4d1f93a027b617020a23a31407ead55
Jimmy Ong <112923324+Jake-Sudoku@users.noreply.github.com>
2022-12-15 14:32:32 +0800

    Update 2_mosquito_cluster.py

--- Steamlit-test/pages/2_mosquito_cluster.py
+++ Steamlit-test/pages/2_mosquito_cluster.py
@@ -11,81 +11,55 @@ from  PIL import Image
 import cv2
 import io
 import base64
+from pathlib import Path
 
 # Set Page configuration
 # Read more at https://docs.streamlit.io/1.6.0/library/api-reference/utilities/st.set_page_config
-st.set_page_config(page_title='Project 4 - West Nile Virus Prediction', page_icon='🦟', layout='wide', initial_sidebar_state='expanded')
+st.set_page_config(page_title='Number of Mosquitos in areas with wnvpresent', page_icon='🦟', layout='wide', initial_sidebar_state='expanded')
 
 # Set title of the app
 st.title('🦟💀 Project 4 - West Nile Virus Prediction')
 
-with st.sidebar:
-    choose = option_menu("Directory",[ "About","Slides", "Interactive"],
-                         icons=['people', 'file earmark slides', 'bar-chart'],
-                         menu_icon="app-indicator", default_index=0,
-                         styles={
-        "container": {"padding": "5!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#02ab21"},
-    }
-    )
+# Load data
+df_train_csv = Path(__file__).parents[0] / 'train_cleaned.csv'
+df_spray_csv = Path(__file__).parents[0] / 'spray_cleaned.csv'
 
-#The "About" page
-if choose == "About":
-    st.markdown(""" <style> .font {
-    font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
-    </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font">About</p>', unsafe_allow_html=True)
-    st.write("This project is done to as part of General's Assembly (GA) requirement to pass the course. \nIt consist of the following sections below : \n\nAbout \n\nSlides \n\nInteractive")    
-    #st.image(profile, width=700 )
+# Load data
+df_train = pd.read_csv(df_train_csv)
+df_spray = pd.read_csv(df_spray_csv)
 
-#The "Slides" page
-elif choose == "Slides":
-    def show_pdf(file_path):
-        with open(file_path,"rb") as f:
-            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-            st.markdown(pdf_display, unsafe_allow_html=True)
-            show_pdf('testGAProject4.pdf')
+#Number of Mosquitos in areas with wnvpresent
+MAPBOX_TOKEN = 'pk.eyJ1IjoibWFyaWVkcmV6IiwiYSI6ImNsOXl5dTFtZjAyYm4zd28zN3Y1ZzYycm0ifQ.W1Toe6X5S9AELY56h0OQDw'
+px.set_mapbox_access_token(MAPBOX_TOKEN)
 
+#Count the number of mosquitos in areas
+mosquito_count = df_train.groupby(['address'], as_index = False)[['nummosquitos']].sum()
+#Group the area by address and use the median address
+areas = df_train.groupby(['address'], as_index = False)[['latitude','longitude']].median()
 
-elif choose == "Interactive":
-    
-    # Load data
-    def data(): 
-        df_train = pd.read_csv('/train_cleaned.csv')
-        return df_train
-    
-    df_train = data()
-    #df_train = pd.read_csv("traincleaned.csv")
-    df_spray = pd.read_csv("spraycleaned.csv")
+#Group the wnvpresent by address
+wnv = df_train.groupby(['address'], as_index = False)[['wnvpresent']].sum() 
 
-    #Spray Locations from 2011-2013
-    MAPBOX_TOKEN = 'pk.eyJ1IjoibWFyaWVkcmV6IiwiYSI6ImNsOXl5dTFtZjAyYm4zd28zN3Y1ZzYycm0ifQ.W1Toe6X5S9AELY56h0OQDw'
-    px.set_mapbox_access_token(MAPBOX_TOKEN)
-    fig = px.scatter_mapbox(df_spray, lat = 'latitude', lon  = 'longitude',animation_frame="date",size_max=15, zoom = 10)
+#Merge mosquitocount , areas and wnvpresent together
+mosquito_areas_wnv = pd.concat([mosquito_count,areas, wnv], axis = 1)
 
-    fig.update_layout(title = 'Spray Locations from 2011-2013',
-                      autosize=False,
-                      width=500,
-                      height=700,
-                      mapbox_accesstoken=MAPBOX_TOKEN,mapbox_style="light")
+#Drop the address as it is not required
+mosquito_areas_wnv.drop('address', axis = 1, inplace = True)
 
-    fig.show()
-    st.plotly_chart(fig, use_container_width=True)
-    
-    
-    #Traps Locations from 2007-2013
-    MAPBOX_TOKEN = 'pk.eyJ1IjoibWFyaWVkcmV6IiwiYSI6ImNsOXl5dTFtZjAyYm4zd28zN3Y1ZzYycm0ifQ.W1Toe6X5S9AELY56h0OQDw'
-    px.set_mapbox_access_token(MAPBOX_TOKEN)
-    fig = px.scatter_mapbox(df_train, lat = 'latitude', lon  = 'longitude',animation_frame="date",size_max=15, zoom = 10)
+fig = px.scatter_mapbox(mosquito_areas_wnv, lat = 'latitude', lon  = 'longitude', color = 'wnvpresent',
+                        size = 'nummosquitos', color_continuous_scale=px.colors.cyclical.Edge,
+                        hover_data = ['nummosquitos', 'wnvpresent'],
+                       zoom = 9,mapbox_style="light",
+                       title="Number of Mosquitos in areas with wnvpresent")
 
-    fig.update_layout(title = 'Traps Locations from 2007-2013',
-    autosize=False,
-    width=500,
-    height=700,)
+fig.show()
+st.plotly_chart(fig, use_container_width=True)
 
-    fig.show()
-    st.plotly_chart(fig, use_container_width=True)
-    
+fig1 = px.scatter_mapbox(mosquito_areas_wnv, lat = 'latitude', lon  = 'longitude', color = 'wnvpresent',
+                        size = 'nummosquitos', color_continuous_scale=px.colors.cyclical.Edge,
+                        hover_data = ['nummosquitos', 'wnvpresent'],
+                       zoom = 9,mapbox_style="open-street-map",
+                       title="Number of Mosquitos in areas with wnvpresent")
+
+fig1.show()
+st.plotly_chart(fig1, use_container_width=True)
